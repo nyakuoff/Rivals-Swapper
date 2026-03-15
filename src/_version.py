ff0fbfe3fd7df10752631dd1355dfead4b8a0f86
nyakuoff <bertrandbridou1994@gmail.com>
2026-03-14 19:42:16 -0500

    bump version to 1.0.4

--- src/_version.py
+++ src/_version.py
@@ -1,7 +1,7 @@
 """Application version and GitHub release-check helpers."""
 from __future__ import annotations
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 GITHUB_REPO  = "nyakuoff/Rivals-Swapper"
 
 
